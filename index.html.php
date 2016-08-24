3d04307e02a60b7c0763fb15bdbb9ab67bf536fc
Jose Leonan Carvalho <j.leonancarvalho@gmail.com>
2016-08-24 19:39:25 -0300

    Seed Structure

--- index.html.php
+++ index.html.php
@@ -6,4 +6,6 @@ include_once './vendor/autoload.php';
 $teste = new \Orion\NGSIAPIv2("ngsi.vm9it.com");
 var_dump($teste->checkStatus());
 
-Application\Util::GenerateTemplate("Teste", "Foo");
+echo \Application\Util::GenerateTemplate("Teste", "Foo");
+?>
+
