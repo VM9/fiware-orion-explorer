5bd7fab92b52047caa243bff7c2d91f21f4ab305
Jose Leonan Carvalho <j.leonancarvalho@gmail.com>
2016-08-25 18:43:47 -0300

    Functional Release

---
+++ lib/Application/Main.php
@@ -0,0 +1,24 @@
+<?php
+namespace Application;
+
+/**
+ * Description of App
+ *
+ * @author Leonan Carvalho
+ */
+class Main  {
+    public static $_config;
+    private static $_privateToken;
+    public  static $_publicToken;
+    
+    public function __construct() {
+        $this->_config = self::getConfig();
+    }
+    
+    public static function getConfig(){
+        if (empty(self::$_config)) {
+            self::$_config = new Config\init();
+        }
+        return self::$_config;
+    }
+}
